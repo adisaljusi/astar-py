5fc339d9b0d20ee2bcd133beea6628e6f0e277af
adisaljusi <adis.aljusi@hotmail.com>
2021-01-05 09:33:01 +0100

    Create A* star algo

---
+++ AStar.py
@@ -0,0 +1,80 @@
+from Constants import adjacent_squares
+from Node import Node
+
+
+def astar(grid, start, end):
+    start_node = Node(None, start)
+    end_node = Node(None, end)
+
+    open_list = []
+    closed_list = []
+
+    open_list.append(start_node)
+
+    while len(open_list) > 0:
+        current_node = open_list[0]
+        current_index = 0
+
+        for index, item in enumerate(open_list):
+            if item.F < current_node.F:
+                current_node = item
+                current_index = index
+
+        open_list.pop(current_index)
+        closed_list.append(current_node)
+
+        if current_node == end_node:
+            path = []
+            current = current_node
+
+            while current is not None:
+                path.append(current.position)
+                current = current.parent
+            return path[::-1]
+
+        children = generate_children(grid, current_node, closed_list)
+
+        for child in children:
+            for closed_child in closed_list:
+                if child == closed_child:
+                    continue
+
+            child.G = current_node.G + 1
+            child.H = (child.position[0] - end_node.position[0] ** 2) + (
+                        (child.position[1] - end_node.position[1]) ** 2)
+            child.F = child.G + child.H
+
+            for open_node in open_list:
+                if child == open_node and child.G > open_node.G:
+                    continue
+
+            open_list.append(child)
+
+
+def generate_children(grid, current_node, closed_list):
+    children = []
+
+    for new_position in adjacent_squares:
+        node_position = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
+
+        if is_node_in_range(grid, node_position):
+            continue
+
+        if grid[node_position[0]][node_position[1]] != 0:
+            continue
+
+        new_node = Node(current_node, node_position)
+        if new_node in closed_list:
+            continue
+
+        children.append(new_node)
+
+    return children
+
+
+def is_node_in_range(grid, node):
+    grid_len = len(grid) - 1
+    n0 = node[0]
+    n1 = node[1]
+
+    return n0 > grid_len or n0 < 0 or n1 > (len(grid[grid_len]) - 1) or n1 < 0
