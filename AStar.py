0125c7f9ebedd2b50576200e621bc99aa300d45b
adisaljusi <adis.aljusi@hotmail.com>
2021-01-05 09:38:23 +0100

    refactor variable names

--- AStar.py
+++ AStar.py
@@ -6,22 +6,22 @@ def astar(grid, start, end):
     start_node = Node(None, start)
     end_node = Node(None, end)
 
-    open_list = []
-    closed_list = []
+    open_nodes = []
+    closed_nodes = []
 
-    open_list.append(start_node)
+    open_nodes.append(start_node)
 
-    while len(open_list) > 0:
-        current_node = open_list[0]
+    while len(open_nodes) > 0:
+        current_node = open_nodes[0]
         current_index = 0
 
-        for index, item in enumerate(open_list):
+        for index, item in enumerate(open_nodes):
             if item.F < current_node.F:
                 current_node = item
                 current_index = index
 
-        open_list.pop(current_index)
-        closed_list.append(current_node)
+        open_nodes.pop(current_index)
+        closed_nodes.append(current_node)
 
         if current_node == end_node:
             path = []
@@ -32,26 +32,26 @@ def astar(grid, start, end):
                 current = current.parent
             return path[::-1]
 
-        children = generate_children(grid, current_node, closed_list)
+        children = generate_children(grid, current_node, closed_nodes)
 
         for child in children:
-            for closed_child in closed_list:
+            for closed_child in closed_nodes:
                 if child == closed_child:
                     continue
 
             child.G = current_node.G + 1
             child.H = (child.position[0] - end_node.position[0] ** 2) + (
-                        (child.position[1] - end_node.position[1]) ** 2)
+                    (child.position[1] - end_node.position[1]) ** 2)
             child.F = child.G + child.H
 
-            for open_node in open_list:
+            for open_node in open_nodes:
                 if child == open_node and child.G > open_node.G:
                     continue
 
-            open_list.append(child)
+            open_nodes.append(child)
 
 
-def generate_children(grid, current_node, closed_list):
+def generate_children(grid, current_node, closed_nodes):
     children = []
 
     for new_position in adjacent_squares:
@@ -64,7 +64,7 @@ def generate_children(grid, current_node, closed_list):
             continue
 
         new_node = Node(current_node, node_position)
-        if new_node in closed_list:
+        if new_node in closed_nodes:
             continue
 
         children.append(new_node)
